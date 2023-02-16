9838b587c0b7478a27f08af761ec2d5e64ce7e0a
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:47:23 -0500

    add setters on car class

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,26 @@
 public class Car {
 
-    private final boolean wheels;
-    private final int cylinders;
+    private boolean wheels;
+    private int cylinders;
 
     public Car(boolean wheels, int cylinders) {
         this.wheels = wheels;
         this.cylinders = cylinders;
+    }
+
+    public boolean isWheels() {
+        return wheels;
+    }
+
+    public void setWheels(boolean wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public int getCylinders() {
+        return cylinders;
     }
 }
