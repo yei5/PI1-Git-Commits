4087de2253ef9f2cb0c2ce9c9f263e3f99ba810d
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:52:46 -0500

    Solcuion conflicto car

--- src/Car.java
+++ src/Car.java
@@ -7,4 +7,27 @@ public class Car {
         this.wheels = wheels;
         this.cylinders = cylinders;
     }
+
+    public boolean isWheels() {
+        return wheels;
+    }
+
+    public void setWheels(boolean wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public String toString() {
+        return "Wheels: " + wheels + "cylinders: " + cylinders + "\n";
+    }
+
 }
+ 
