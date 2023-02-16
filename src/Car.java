c2c96c27352dde022b250378a7bc7a7e62029ed4
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:43:10 -0500

    add car class

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+
+    private final boolean wheels;
+    private final int cylinders;
+
+    public Car(boolean wheels, int cylinders) {
+        this.wheels = wheels;
+        this.cylinders = cylinders;
+    }
+}
