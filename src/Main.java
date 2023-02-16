6e75cd7b1ff4b6b04cab0cf0fb5c0b12190fc435
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:50:19 -0500

    add list on main and add cars on for loop

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            //FOR INUTIL
-            System.out.println(i);
+            cars.add(new Car(true, i));
         }
-        System.out.println("Hello world!");
+        System.out.println("Ahh");
     }
 }
