a9853548200230aae5efefc0db34a9b827593e0e
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:40:03 -0500

    add for loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            //FOR INUTIL
+            System.out.println(i);
+        }
         System.out.println("Hello world!");
     }
 }
